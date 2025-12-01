4702cbe464a261816ccb29cf92e50f5832809240
Casey Watson <casey.watson@tanium.com>
2025-12-01 16:46:15 -0700

    add last builtin

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -3,6 +3,22 @@ package evaluator
 import "github.com/watsoncj/monkey/object"
 
 var builtins = map[string]*object.Builtin{
+	"last": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			switch arg := args[0].(type) {
+			case *object.Array:
+				if len(arg.Elements) == 0 {
+					return NULL
+				}
+				return arg.Elements[len(arg.Elements)-1]
+			default:
+				return newError("argument to `last` not supported, got %s", args[0].Type())
+			}
+		},
+	},
 	"len": &object.Builtin{
 		Fn: func(args ...object.Object) object.Object {
 			if len(args) != 1 {
